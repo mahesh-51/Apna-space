0fa75d256fee0c6ceb777aac4e0dd168dd0ad42e
Kuldeep <kuldeep.mentem@gmail.com>
2024-02-07 16:43:25 +0530

    set google map to sardaprpura

--- application/views/contact.php
+++ application/views/contact.php
@@ -5,7 +5,7 @@
                     <div class="col-12 p-0">
                         <div class="card map border-0">
                             <div class="card-body p-0">
-                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d39206.002432144705!2d-95.4973981212445!3d29.709510002925988!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8640c16de81f3ca5%3A0xf43e0b60ae539ac9!2sGerald+D.+Hines+Waterwall+Park!5e0!3m2!1sen!2sin!4v1566305861440!5m2!1sen!2sin" style="border:0" allowfullscreen></iframe>
+                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14310.032787057271!2d72.99879910346411!3d26.27762048249938!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39418c311b075dc3%3A0x20110ffc37f13131!2sSardarpura%2C%20Jodhpur%2C%20Rajasthan%20342003!5e0!3m2!1sen!2sin!4v1707303721917!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                             </div>
                         </div>
                     </div><!--end col-->
@@ -74,8 +74,8 @@
                             </div>
                             <div class="content ms-4">
                                 <h5>Phone</h5>
-                                <p class="text-muted">Start working with Codash that can provide everything</p>
-                                <a href="tel:+152534-468-854" class="text-primary">+152 534-468-854</a>
+                                <p class="text-muted">Start working with Apna-Space that can provide everything</p>
+                                <a href="tel:+152534-468-854" class="text-primary">+91-8806920771</a>
                             </div>
                         </div>
                     </div><!--end col-->
@@ -87,8 +87,8 @@
                             </div>
                             <div class="content ms-4">
                                 <h5>Email</h5>
-                                <p class="text-muted">Start working with Codash that can provide everything</p>
-                                <a href="mailto:contact@example.com" class="text-primary">contact@example.com</a>
+                                <p class="text-muted">Start working with Apna-Space that can provide everything</p>
+                                <a href="mailto:Mahesh.mentem@gmail.com" class="text-primary">Mahesh.mentem@gmail.com</a>
                             </div>
                         </div>
                     </div><!--end col-->
@@ -100,8 +100,8 @@
                             </div>
                             <div class="content ms-4">
                                 <h5>Location</h5>
-                                <p class="text-muted">C/54 Northwest Freeway, Suite 558, Houston, USA 485</p>
-                                <a href="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d39206.002432144705!2d-95.4973981212445!3d29.709510002925988!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8640c16de81f3ca5%3A0xf43e0b60ae539ac9!2sGerald+D.+Hines+Waterwall+Park!5e0!3m2!1sen!2sin!4v1566305861440!5m2!1sen!2sin" class="video-play-icon text-primary">View on Google map</a>
+                                <p class="text-muted">Sardarpura B road, Jodhpur, Rajasthan, India</p>
+                                <a href="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14310.032787057271!2d72.99879910346411!3d26.27762048249938!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39418c311b075dc3%3A0x20110ffc37f13131!2sSardarpura%2C%20Jodhpur%2C%20Rajasthan%20342003!5e0!3m2!1sen!2sin!4v1707303721917!5m2!1sen!2sin" target="_blank" class="video-play-icon text-primary">View on Google map</a>
                             </div>
                         </div>
                     </div><!--end col-->
